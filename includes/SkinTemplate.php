c11a7e28bc572530e4e9822957dd73d0d5f04fb8
Wikimedia Foundation <wikimedia@github.com>
2012-01-11 21:59:55 +0000

    MediaWiki 1.18.1

--- includes/SkinTemplate.php
+++ includes/SkinTemplate.php
@@ -480,7 +480,9 @@ class SkinTemplate extends Skin {
 						'text' => ( $wgContLang->getLanguageName( $nt->getInterwiki() ) != '' ?
 									$wgContLang->getLanguageName( $nt->getInterwiki() ) : $l ),
 						'title' => $nt->getText(),
-						'class' => $class
+						'class' => $class,
+						'lang' => $nt->getInterwiki(),
+						'hreflang' => $nt->getInterwiki(),
 					);
 				}
 			}
@@ -851,7 +853,7 @@ class SkinTemplate extends Skin {
 		wfRunHooks( 'SkinTemplatePreventOtherActiveTabs', array( &$this, &$preventActiveTabs ) );
 
 		// Checks if page is some kind of content
-		if( $title->getNamespace() != NS_SPECIAL ) {
+		if( $title->canExist() ) {
 			// Gets page objects for the related namespaces
 			$subjectPage = $title->getSubjectPage();
 			$talkPage = $title->getTalkPage();
